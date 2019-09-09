8bc2825bdfcb63610314bf2bd5214539c92d84bf
Lucas <gris@alunos.utfpr.edu.br>
2019-09-09 09:20:10 -0300

    Update model

--- model.py
+++ model.py
@@ -1,11 +1,12 @@
 from keras.models import Model
 from keras.optimizers import Adam
 from keras.layers import Conv2D, MaxPooling2D, Dropout, Dense,\
-                         GlobalAveragePooling2D, Input
+    GlobalAveragePooling2D, Input
 
-from common.config import Config    
+from common.config import Config
 
 # Model based on https://github.com/tomasz-oponowicz/spoken_language_identification
+
 
 def build_model(conf: Config, space, input_shape) -> Model:
     input_data = Input(shape=input_shape)
@@ -14,105 +15,107 @@ def build_model(conf: Config, space, input_shape) -> Model:
         kernel_size=3,
         activation='relu',
         data_format='channels_last',
-        padding=('same' if space['maxpooling_1'] is not None else
-                 'valid')
+        padding=('same' if space['maxpooling_1'] is not None else 'valid')
     )(input_data)
     if space['maxpooling_1'] is not None:
-        x = MaxPooling2D(pool_size=2)(x)
+        x = MaxPooling2D(pool_size=2, padding='same')(x)
     x = Dropout(space['dropout1'])(x)
 
     if space['filters_conv2d_2'] is not None:
         x = Conv2D(
-        filters=space['filters_conv2d_2'],
-                kernel_size=3,
-                activation='relu',
-                padding=('same' if space['maxpooling_2'] is not None else
-                         'valid')
+            filters=space['filters_conv2d_2'],
+            kernel_size=3,
+            activation='relu',
+            padding=('same' if space['maxpooling_2'] is not None else
+                     'valid')
         )(x)
         if space['maxpooling_2'] is not None:
-            x = MaxPooling2D(pool_size=2)(x)
+            x = MaxPooling2D(pool_size=2, padding='same')(x)
         x = Dropout(space['dropout2'])(x)
 
     if space['filters_conv2d_3'] is not None:
         x = Conv2D(
-        filters=space['filters_conv2d_3'],
-                kernel_size=3,
-                activation='relu',
-                padding=('same' if space['maxpooling_3'] is not None else
-                         'valid')
+            filters=space['filters_conv2d_3'],
+            kernel_size=3,
+            activation='relu',
+            padding=('same' if space['maxpooling_3'] is not None else
+                     'valid')
         )(x)
         if space['maxpooling_3'] is not None:
-            x = MaxPooling2D(pool_size=2)(x)
-        x = Dropout(0.2)(x)
-    
+            x = MaxPooling2D(pool_size=2, padding='same')(x)
+        x = Dropout(space['dropout3'])(x)
+
     if space['filters_conv2d_4'] is not None:
         x = Conv2D(
-        filters=space['filters_conv2d_4'],
-                kernel_size=3,
-                activation='relu',
-                padding=('same' if space['maxpooling_4'] is not None else
-                         'valid')
+            filters=space['filters_conv2d_4'],
+            kernel_size=3,
+            activation='relu',
+            padding=('same' if space['maxpooling_4'] is not None else
+                     'valid')
         )(x)
         if space['maxpooling_4'] is not None:
-            x = MaxPooling2D(pool_size=2)(x)
-        x = Dropout(0.2)(x)
-    
+            x = MaxPooling2D(pool_size=2, padding='same')(x)
+        x = Dropout(space['dropout4'])(x)
+
     if space['filters_conv2d_5'] is not None:
         x = Conv2D(
-        filters=space['filters_conv2d_5'],
-                kernel_size=3,
-                activation='relu',
-                padding=('same' if space['maxpooling_5'] is not None else
-                         'valid')
+            filters=space['filters_conv2d_5'],
+            kernel_size=3,
+            activation='relu',
+            padding=('same' if space['maxpooling_5'] is not None else
+                     'valid')
         )(x)
         if space['maxpooling_5'] is not None:
-            x = MaxPooling2D(pool_size=2)(x)
-        x = Dropout(0.2)(x)
-    
+            x = MaxPooling2D(pool_size=2, padding='same')(x)
+        x = Dropout(space['dropout5'])(x)
+
     if space['filters_conv2d_6'] is not None:
         x = Conv2D(
-        filters=space['filters_conv2d_6'],
-                kernel_size=3,
-                activation='relu',
-                padding=('same' if space['maxpooling_6'] is not None else
-                         'valid')
+            filters=space['filters_conv2d_6'],
+            kernel_size=3,
+            activation='relu',
+            padding=('same' if space['maxpooling_6'] is not None else
+                     'valid')
         )(x)
         if space['maxpooling_6'] is not None:
-            x = MaxPooling2D(pool_size=2)(x)
-        x = Dropout(0.2)(x)
-    
+            x = MaxPooling2D(pool_size=2, padding='same')(x)
+        x = Dropout(space['dropout6'])(x)
+
     if space['filters_conv2d_7'] is not None:
         x = Conv2D(
-        filters=space['filters_conv2d_7'],
-                kernel_size=3,
-                activation='relu',
-                padding=('same' if space['maxpooling_7'] is not None else
-                        'valid')
+            filters=space['filters_conv2d_7'],
+            kernel_size=3,
+            activation='relu',
+            padding=('same' if space['maxpooling_7'] is not None else'valid')
         )(x)
         if space['maxpooling_7'] is not None:
-            x = MaxPooling2D(pool_size=2)(x)
-        x = Dropout(0.2)(x)
-    
+            x = MaxPooling2D(pool_size=2, padding='same')(x)
+        x = Dropout(space['dropout7'])(x)
+
     if space['filters_conv2d_8'] is not None:
         x = Conv2D(
-        filters=space['filters_conv2d_8'],
-                kernel_size=3,
-                activation='relu',
-                padding=('same' if space['maxpooling_8'] is not None else
-                        'valid')
+            filters=space['filters_conv2d_8'],
+            kernel_size=3,
+            activation='relu',
+            padding=('same' if space['maxpooling_8'] is not None else'valid')
         )(x)
         if space['maxpooling_8'] is not None:
-            x = MaxPooling2D(pool_size=2)(x)
-        x = Dropout(0.2)(x)
+            x = MaxPooling2D(pool_size=2, padding='same')(x)
+        x = Dropout(space['dropout8'])(x)
 
     x = GlobalAveragePooling2D()(x)
 
     if space['dense_1'] is not None:
         x = Dense(space['dense_1'], activation='relu')(x)
+        x = Dropout(space['dropout_dense'])(x)
     if space['dense_2'] is not None:
         x = Dense(space['dense_2'], activation='relu')(x)
+        x = Dropout(space['dropout_dense'])(x)
+    if space['dense_3'] is not None:
+        x = Dense(space['dense_3'], activation='relu')(x)
+        x = Dropout(space['dropout_dense'])(x)
 
-    x = Dense(conf.num_classes, activation='softmax')(x)
+    x = Dense(conf.num_classes, activation='softmax', name='dense_softmax')(x)
     model = Model(input_data, x)
     opt = space['opt'](lr=space['lr'])
     model.compile(loss='categorical_crossentropy',
